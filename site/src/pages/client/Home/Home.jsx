9aba7c66deca0a3dff5648769b1e98d4b5974fd3
Sadiqovilkin <sadiqovilkin068@gmail.com>
2024-06-27 18:48:05 +0400

    Credit Calcuator Added

--- site/src/pages/client/Home/Home.jsx
+++ site/src/pages/client/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Slider from './Slider/Slider'
 import AboutUs from '../About/AboutUs/AboutUs'
+import OurServices from './OurSevices/OurServices'
+import Calcuator from './Calcuator/Calcuator'
 
 const Home = () => {
   return (
     <main>
       <Slider/>
       <AboutUs/>
+      <OurServices/>
+      <Calcuator/>
     </main>
   )
 }
