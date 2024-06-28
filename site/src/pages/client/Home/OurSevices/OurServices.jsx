a6c527a5f4d85624800122184ab36848b38ddfba
Sadiqovilkin <sadiqovilkin068@gmail.com>
2024-06-28 13:49:52 +0400

    #our Services Sections Added

--- site/src/pages/client/Home/OurSevices/OurServices.jsx
+++ site/src/pages/client/Home/OurSevices/OurServices.jsx
@@ -1,8 +1,151 @@
 import React from 'react'
-
+import { BsBank } from "react-icons/bs";
 const OurServices = () => {
   return (
-    <div>OurServices</div>
+   <section id='ourServices'>
+    <div className="container-fluid">
+      <div className="row py-5 justify-content-center">
+        <div className="col-lg-6">
+      <div className="section_header">
+      <h6>Our Services</h6>
+          <h2>Online Banking at Your
+          Fingertips</h2>
+      </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-3 g-4 col-md-4 col-sm-6 col-12">
+          <div className="serviceCard">
+            <div className="card_icon">
+              <BsBank/>
+            </div>
+            <div className="card_text">
+              <h3>
+              Digital Banking</h3>
+              <ul>
+                <li>Bank&Saving accaounts</li>
+                <li>Credit Cards</li>
+                <li>personal Loans</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 g-4 col-md-4 col-sm-6 col-12">
+          <div className="serviceCard">
+            <div className="card_icon">
+              <BsBank/>
+            </div>
+            <div className="card_text">
+              <h3>
+              Digital Banking</h3>
+              <ul>
+                <li>Bank&Saving accaounts</li>
+                <li>Credit Cards</li>
+                <li>personal Loans</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 g-4 col-md-4 col-sm-6 col-12">
+          <div className="serviceCard">
+            <div className="card_icon">
+              <BsBank/>
+            </div>
+            <div className="card_text">
+              <h3>
+              Digital Banking</h3>
+              <ul>
+                <li>Bank&Saving accaounts</li>
+                <li>Credit Cards</li>
+                <li>personal Loans</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 g-4 col-md-4 col-sm-6 col-12">
+          <div className="serviceCard">
+            <div className="card_icon">
+              <BsBank/>
+            </div>
+            <div className="card_text">
+              <h3>
+              Digital Banking</h3>
+              <ul>
+                <li>Bank&Saving accaounts</li>
+                <li>Credit Cards</li>
+                <li>personal Loans</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 g-4 col-md-4 col-sm-6 col-12">
+          <div className="serviceCard">
+            <div className="card_icon">
+              <BsBank/>
+            </div>
+            <div className="card_text">
+              <h3>
+              Digital Banking</h3>
+              <ul>
+                <li>Bank&Saving accaounts</li>
+                <li>Credit Cards</li>
+                <li>personal Loans</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 g-4 col-md-4 col-sm-6 col-12">
+          <div className="serviceCard">
+            <div className="card_icon">
+              <BsBank/>
+            </div>
+            <div className="card_text">
+              <h3>
+              Digital Banking</h3>
+              <ul>
+                <li>Bank&Saving accaounts</li>
+                <li>Credit Cards</li>
+                <li>personal Loans</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 g-4 col-md-4 col-sm-6 col-12">
+          <div className="serviceCard">
+            <div className="card_icon">
+              <BsBank/>
+            </div>
+            <div className="card_text">
+              <h3>
+              Digital Banking</h3>
+              <ul>
+                <li>Bank&Saving accaounts</li>
+                <li>Credit Cards</li>
+                <li>personal Loans</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-3 g-4 col-md-4 col-sm-6 col-12">
+          <div className="serviceCard">
+            <div className="card_icon">
+              <BsBank/>
+            </div>
+            <div className="card_text">
+              <h3>
+              Digital Banking</h3>
+              <ul>
+                <li>Bank&Saving accaounts</li>
+                <li>Credit Cards</li>
+                <li>personal Loans</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+   </section>
   )
 }
 
